5b265fbf20f9f8cd09a28e198264ceade7d89c46
Jake Langholz <jake@shareinteractive.com>
2016-05-19 14:45:45 -0700

    Completed auth, implementing check on home page.

--- app/src/main/java/com/bcgdv/langholzjacob/baseapp/HomeActivity.java
+++ app/src/main/java/com/bcgdv/langholzjacob/baseapp/HomeActivity.java
@@ -2,6 +2,7 @@ package com.bcgdv.langholzjacob.baseapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -14,12 +15,18 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        loginFragment = new LoginFragment();
+
         FirebaseAuth auth = FirebaseAuth.getInstance();
         if (auth.getCurrentUser() == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(android.R.id.content, loginFragment).commit();
         } else {
+            TextView user_email = (TextView) findViewById(R.id.user_email);
+            TextView firebase_uid = (TextView) findViewById(R.id.firebase_uid);
 
+            user_email.setText(auth.getCurrentUser().getEmail());
+            firebase_uid.setText(auth.getCurrentUser().getUid());
         }
 
 
--- app/src/main/java/com/bcgdv/langholzjacob/baseapp/LoginFragment.java
+++ app/src/main/java/com/bcgdv/langholzjacob/baseapp/LoginFragment.java
@@ -3,14 +3,21 @@ package com.bcgdv.langholzjacob.baseapp;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 /**
@@ -23,7 +30,7 @@ import com.google.firebase.auth.FirebaseAuth;
  */
 public class LoginFragment extends Fragment {
 
-    private static FirebaseAuth m_auth;
+    private FirebaseAuth m_auth;
     private EditText m_usernameEditText;
     private EditText m_passwordEditText;
     private Button m_signInButton;
@@ -46,27 +53,6 @@ public class LoginFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         m_auth = FirebaseAuth.getInstance();
-
-        m_usernameEditText = (EditText) getView().findViewById(R.id.username);
-        m_passwordEditText = (EditText) getView().findViewById(R.id.password);
-        m_signInButton = (Button) getView().findViewById(R.id.sign_in);
-        m_signUpButton = (Button) getView().findViewById(R.id.sign_up);
-
-        m_signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                m_auth.signInWithEmailAndPassword(m_usernameEditText.getText().toString(),
-                        m_passwordEditText.getText().toString());
-            }
-        });
-
-        m_signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                m_auth.createUserWithEmailAndPassword(m_usernameEditText.getText().toString(),
-                        m_passwordEditText.getText().toString());
-            }
-        });
     }
 
     @Override
@@ -75,18 +61,19 @@ public class LoginFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
 
-
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onSignInButtonPressed() {
-
-        m_auth.signInWithEmailAndPassword("", "");
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (initViews()) {
+            applyOnClickListeners();
+        }
     }
 
     @Override
     public void onAttach(Context context) {
-
+        super.onAttach(context);
     }
 
     @Override
@@ -107,5 +94,64 @@ public class LoginFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private boolean initViews() {
+        try {
+            m_usernameEditText = (EditText) getView().findViewById(R.id.username);
+            m_passwordEditText = (EditText) getView().findViewById(R.id.password);
+            m_signInButton = (Button) getView().findViewById(R.id.sign_in);
+            m_signUpButton = (Button) getView().findViewById(R.id.sign_up);
+            return true;
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private void applyOnClickListeners() {
+        m_signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                checkEmailAndPassword();
+                m_auth.signInWithEmailAndPassword(m_usernameEditText.getText().toString(),
+                        m_passwordEditText.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        exitFragment(task);
+                    }
+                });
+            }
+        });
+
+        m_signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                m_auth.createUserWithEmailAndPassword(m_usernameEditText.getText().toString(),
+                        m_passwordEditText.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        exitFragment(task);
+                    }
+                });
+
+            }
+        });
+    }
+
+    private void checkEmailAndPassword() {
+        String email = m_usernameEditText.getText().toString();
+        String password = m_passwordEditText.getText().toString();
+        if (!email.contains("@") || !email.contains(".")) {
+            m_usernameEditText.setError(getString(R.string.invalid_email_address));
+        } else if (password.length() < 6) {
+            m_passwordEditText.setError(getString(R.string.invalid_password));
+        }
+    }
+
+    private void exitFragment(@NonNull Task<AuthResult> task) {
+        if (task.isSuccessful()) {
+            getActivity().getSupportFragmentManager().popBackStack();
+        }
     }
 }
