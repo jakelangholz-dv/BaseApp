7119ac595264e49ed62a7dc18f13c46f8e8d5b77
Jake Langholz <jake@shareinteractive.com>
2016-05-19 11:44:25 -0700

    Removed LoginActivity and added LoginFragment. Deciding on how to implement auth, either through fragment or SignInIntentBuilder.

--- app/src/main/java/com/bcgdv/langholzjacob/baseapp/HomeActivity.java
+++ app/src/main/java/com/bcgdv/langholzjacob/baseapp/HomeActivity.java
@@ -3,11 +3,23 @@ package com.bcgdv.langholzjacob.baseapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private LoginFragment loginFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(android.R.id.content, loginFragment).commit();
+        }
+
+
     }
 }
